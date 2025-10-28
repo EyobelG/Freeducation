af84c2a49cb614b0957cdf3c438e1768a368aff2
EyobelG <eyobel.gebre@tufts.edu>
2025-10-28 18:55:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-
-// // LOAD FOOTER ONTO PAGE
-// document.addEventListener("DOMContentLoaded", () => {
-//     const footerContainer = document.getElementById("footer-container");
-  
-//     if (footerContainer) {
-//       fetch("footer.html")
-//         .then(response => response.text())
-//         .then(data => {
-//           footerContainer.innerHTML = data;
-//         })
-//         .catch(error => console.error("Error loading footer:", error));
-//     }
-// });
-  
-
-// In script.js
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('footer-container').innerHTML = `
         <footer class="site-footer">
